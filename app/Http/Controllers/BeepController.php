ee406cf9f76ae9e230bff2aa348bbf0a10cd21d4
Mayowa Ade <made00730@yahoo.co.uk>
2018-04-04 13:05:17 +0100

    Initial Commit

---
+++ app/Http/Controllers/BeepController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class BeepController extends SecureController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
